ef4304fe891ea6d4fdc04cae1864bd3d901d2aaa
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 20:29:44 +0100

    AsyncExtension - change Extension class.

--- src/DependencyInjection/AsyncExtension.php
+++ src/DependencyInjection/AsyncExtension.php
@@ -3,9 +3,9 @@
 namespace Danilovl\AsyncBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class AsyncExtension extends Extension
 {
